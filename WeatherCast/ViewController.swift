34469a604cce5df6de10e506cf1cf5436385be31
Ketul Chauhan <k_chauhan179766@fanshaweonline.ca>
2023-08-01 12:53:47 -0400

    Initial Commit

---
+++ WeatherCast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherCast
+//
+//  Created by Ketul Chauhan on 2023-08-01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
