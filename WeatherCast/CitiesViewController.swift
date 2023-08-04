1c1fa23d63294a1c9c6f0e4891db8cd3d7bbf612
Ketul Chauhan <k_chauhan179766@fanshaweonline.ca>
2023-08-03 22:59:00 -0400

    Redesigned Home Page And Also Added Segue For Second Screen With TableView, Also Connected UIElements to ViewController File

---
+++ WeatherCast/CitiesViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CitiesViewController.swift
+//  WeatherCast
+//
+//  Created by Ketul Chauhan on 2023-08-03.
+//
+
+import UIKit
+
+class CitiesViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
--- WeatherCast/ViewController.swift
+++ WeatherCast/ViewController.swift
@@ -8,12 +8,65 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //User selected temperature unit
+    var currentTemperatureUnit = ""
+    var currentWeatherTemp = -1
+    var currentWeatherInCelsius = 0.0
+    var currentWeatherInFahrenheit = 0.0
 
+    @IBOutlet weak var searchField: UIButton!
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var weatherLabel: UILabel!
+    @IBOutlet weak var currentTemprature: UILabel!
+    @IBOutlet weak var currentTemperatureUnitLabel: UILabel!
+    @IBOutlet weak var currentTempratureUnitButton: UIButton!
+    @IBOutlet weak var cityLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Default temp unit
+        currentTemperatureUnit = "celsius"
     }
 
-
+    @IBAction func temperatureUnitChangePressed(_ sender: UIButton) {
+        if(currentTemperatureUnit == "celsius"){
+            currentTemperatureUnit = "fahrenheit"
+            currentTemperatureUnitLabel.text = "Change Unit To Celsius"
+            currentTempratureUnitButton.setTitle("C", for: .normal)
+            currentTempratureUnitButton.backgroundColor = UIColor.systemOrange
+            currentTemperatureUnitLabel.textColor = UIColor.systemOrange
+            
+            if(currentWeatherTemp != -1){
+                currentTemprature.text = "\(currentWeatherInFahrenheit)\("º F")"
+            }
+            
+        }else{
+            currentTemperatureUnit = "celsius"
+            currentTemperatureUnitLabel.text = "Change Unit To Fahrenheit"
+            currentTempratureUnitButton.setTitle("F", for: .normal)
+            currentTempratureUnitButton.backgroundColor = UIColor.systemBlue
+            currentTemperatureUnitLabel.textColor = UIColor.systemBlue
+            
+            if(currentWeatherTemp != -1){
+                currentTemprature.text = "\(currentWeatherInCelsius)\("º C")"
+            }
+        }
+    }
+    
+    
+    @IBAction func searchIconPressed(_ sender: UIButton) {
+        print("Search Button Pressed")
+    }
+    
+    
+    @IBAction func currentLocationIconPressed(_ sender: UIButton) {
+        print("Current Location Icon Pressed")
+    }
+    
+    
+    @IBAction func citiesButtonPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "goToCitiesScreen", sender: self)
+    }
 }
 
