0de3091a6f6b397cb3fc4286849519827af9524d
Dhruv Patel <dhruv792000patel@gmail.com>
2023-08-04 01:13:30 -0400

    Added Current Location Coordinates Fetch Code With Fetching Data From API For Current Location's Coordinates

--- WeatherCast/ViewController.swift
+++ WeatherCast/ViewController.swift
@@ -6,8 +6,12 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, CLLocationManagerDelegate {
+    //API Key
+    let key = "1aab60093432467ba0f204559230108"
+    
     //User selected temperature unit
     var currentTemperatureUnit = ""
     var currentWeatherTemp = -1
@@ -22,6 +26,9 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var currentTempratureUnitButton: UIButton!
     @IBOutlet weak var cityLabel: UILabel!
     
+    //Location manager for current location
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -30,6 +37,9 @@ class ViewController: UIViewController, UITextFieldDelegate {
         
         //Using delegate for return click on the keyboard
         searchField.delegate = self
+        
+        //Location manager delegate
+        locationManager.delegate = self
     }
 
     @IBAction func temperatureUnitChangePressed(_ sender: UIButton) {
@@ -71,12 +81,57 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
     
     @IBAction func currentLocationIconPressed(_ sender: UIButton) {
-        print("Current Location Icon Pressed")
+        //Requesting location permission
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
     }
     
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last{
+            let lat = location.coordinate.latitude
+            let lon = location.coordinate.longitude
+            let url = fetchWeatherURL(cityOrCoordinates: "\(lat),\(lon)")
+            fetchWeatherData(urlInput: url)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Location manager error: \(error)")
+    }
     
     @IBAction func citiesButtonPressed(_ sender: UIButton) {
         performSegue(withIdentifier: "goToCitiesScreen", sender: self)
+    }
+    
+    //This function will return the url with the city or the coordinate
+    func fetchWeatherURL(cityOrCoordinates: String) -> String{
+        let baseURL = "https://api.weatherapi.com/v1/current.json?key=\(key)&q=\(cityOrCoordinates)"
+        return baseURL
+    }
+    
+    //This function will fetch JSON data from the API
+    func fetchWeatherData(urlInput:String){
+        //1. Create a URL
+        if let url = URL(string: urlInput){
+            //2. Create a URLSession
+            let session = URLSession(configuration: .default)
+            
+            //3. Give the session a task
+            let task = session.dataTask(with: url, completionHandler: {(data, response, error) in
+                if error != nil{
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    print("Data Fetched From URL: \(safeData)")
+                    //TODO: Create A Function That Parses JSON and Update UI Elements.....
+                }
+            })
+            
+            //4. Start the task
+            task.resume()
+        }
     }
 }
 
